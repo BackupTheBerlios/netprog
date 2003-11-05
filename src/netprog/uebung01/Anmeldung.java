1a37964a3b6f415b64c90d785cc539ab9d0d8310
secos <secos>
2003-11-05 13:24:05 +0000

    Initial revision

---
+++ src/netprog/uebung01/Anmeldung.java
@@ -0,0 +1,16 @@
+package netprog.uebung01;
+
+public class Anmeldung
+{
+    public Anmeldung()
+    {
+        System.out.println("Will be implemented when project work starts on 3/11/2003.");
+    }
+
+    public static void main(String[] args)
+    {
+        //new Anmeldung();
+	
+    }
+
+}
