9d907afef309f017759190e656ade2966c804ac3
secos <secos>
2003-11-05 16:08:33 +0000

    <Kein Kommentar eingegeben>

--- src/netprog/uebung01/Eintrag.java
+++ src/netprog/uebung01/Eintrag.java
@@ -5,14 +5,20 @@ import java.rmi.*;
 public class Eintrag
 {
     public Eintrag()
-    {
-        try
-        {
-            Gruppenliste c = (Gruppenliste)java.rmi.Naming.lookup("rmi://nawab.inf.fu-berlin.de/liste");
+    {   try
+        {   Gruppenliste c = (Gruppenliste)java.rmi.Naming.lookup("rmi://nawab.inf.fu-berlin.de/liste");
             c.loesche(6);
             c.trageEin(6,"Sebastian Koske","3500729","koske@inf.fu-berlin.de");
             System.out.println(c.toString(6));
             c.loesche(6);
+
+            /*  Ich bekommen immer folgende Exception:
+
+                java.rmi.UnmarshalException: error unmarshalling return; nested exception is:
+	            java.lang.ClassNotFoundException: GruppenlisteServer_Stub (no security manager: RMI class loader disabled)
+
+                keine Ahnung, ob das mein Fehler ist, oder ob es am Server liegt, hat jemand ne Idee?
+            */
         }
         catch (NotBoundException e)
         {
@@ -31,7 +37,7 @@ public class Eintrag
     public static void main(String[] args)
     {
         new Eintrag();
-	
+
     }
 
 }
--- src/netprog/uebung01/Gruppenliste.java
+++ src/netprog/uebung01/Gruppenliste.java
@@ -1,6 +1,9 @@
 package netprog.uebung01;
 
-public interface Gruppenliste extends java.rmi.Remote
+import java.rmi.Remote;
+
+public interface Gruppenliste
+extends Remote
 {
     public void loesche(int gruppenNummer)
     throws java.rmi.RemoteException;
@@ -8,6 +11,6 @@ public interface Gruppenliste extends java.rmi.Remote
     public void trageEin(int gruppenNummer, String name, String matrikel, String email)
     throws java.rmi.RemoteException;
 
-    public String[] toString(int gruppenNummer) 
+    public String[] toString(int gruppenNummer)
     throws java.rmi.RemoteException;
 }
