b7e4188c67fff1bfea47b5b466196702c7543677
secos <secos>
2003-11-07 16:04:25 +0000

    <Kein Kommentar eingegeben>

---
+++ src/netprog/uebung01/a4/GesamtVektor.java
@@ -0,0 +1,12 @@
+package netprog.uebung01.a4;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Sebastian Koske
+ * Date: 07.11.2003
+ * Time: 16:53:37
+ * To change this template use Options | File Templates.
+ */
+public interface GesamtVektor
+{
+}
