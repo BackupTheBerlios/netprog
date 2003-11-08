9fe00f49daf7212388fcbcf28dd8d3a19cf39465
secos <secos>
2003-11-08 15:32:33 +0000

    <Kein Kommentar eingegeben>

--- src/netprog/uebung01/a3/ReplicatedStringImpl.java
+++ src/netprog/uebung01/a3/ReplicatedStringImpl.java
@@ -6,35 +6,48 @@ public class ReplicatedStringImpl
 extends java.rmi.server.UnicastRemoteObject
 implements ReplicatedString
 {
-    /************************************************
-     |  Instanzvariablen:
-     *************************************************/
+    //------------------------------------------------
+    //  Instanzvariablen:
+    //------------------------------------------------
 
     private String s;
     private ReplicatedString replicate;
 
-    /************************************************
-     |  Konstruktoren:
-     *************************************************/
+    //------------------------------------------------
+    //  Konstruktoren:
+    //------------------------------------------------
 
     public ReplicatedStringImpl()
     throws RemoteException
     {
     }
 
-    /************************************************
-     |  sondierende Methoden:
-     *************************************************/
+    //------------------------------------------------
+    //  sondierende Methoden:
+    //------------------------------------------------
 
+    /**
+     * @return the stored String previously set by {@link set(String)}.
+     */
     public String get()
     {
         return s;
     }
 
-    /************************************************
-     |  Prozeduren:
-     *************************************************/
+    //------------------------------------------------
+    //  Prozeduren:
+    //------------------------------------------------
 
+    /**
+     * Stores the given String and replicates it if another ReplicateString has been set
+       by using {@link netprog.uebung01.a3.ReplicatedStringImpl#replicateAt(ReplicatedString)
+       replicateAt(ReplicatedString s)}.
+     * The given String may be null.
+     * Note that calling this method may cause a StackOverflow when a cycle has been created.
+     *
+     * @param s the string to be stored and replicated.
+     * @throws RemoteException
+     */
     public void set(String s)
     throws RemoteException
     {
@@ -42,6 +55,12 @@ implements ReplicatedString
         if (replicate != null) replicate.set(s);
     }
 
+    /**
+     * Sets the {@link ReplicatedString} where any String given as parameter for {@link set(String)} should be replicated.
+     * The Object may be null causing no replication.
+     * 
+     * @param rs the Object where the String should be replicated.
+     */
     public void replicateAt(ReplicatedString rs)
     {
         replicate = rs;
