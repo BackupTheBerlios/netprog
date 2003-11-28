8f06050f4d0ae3dc2f0e3dea053e6ecc37a7a0a1
secos <secos>
2003-11-28 04:16:38 +0000

    <Kein Kommentar eingegeben>

--- Uebung02/src/uebung02/CorbaManager.java
+++ Uebung02/src/uebung02/CorbaManager.java
@@ -1,11 +1,11 @@
 package uebung02;
 
-import org.omg.CORBA.ORB;
+import org.omg.CORBA.*;
 import org.omg.CORBA.ORBPackage.InvalidName;
 import org.omg.CosNaming.*;
 import org.omg.CosNaming.NamingContextPackage.*;
 import org.omg.PortableServer.*;
-import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
+import org.omg.PortableServer.POAManagerPackage.*;
 import org.omg.PortableServer.POAPackage.*;
 
 public class CorbaManager
@@ -22,7 +22,8 @@ public class CorbaManager
      * @throws NotFound
      */
     public static org.omg.CORBA.Object getRemoteObject(String host, String name)
-    throws InvalidName, CannotProceed, org.omg.CosNaming.NamingContextPackage.InvalidName, NotFound
+        throws InvalidName, CannotProceed,
+        org.omg.CosNaming.NamingContextPackage.InvalidName, NotFound
     {
         String[] args = {"-ORBInitialPort", "3000", "-ORBInitialHost", host};
 
@@ -48,10 +49,14 @@ public class CorbaManager
      * @throws NotFound
      * @throws CannotProceed
      */
-    public static void installRemoteObjectAtLocalhost(Servant servant, String name)
-    throws InvalidName, AdapterInactive, WrongPolicy, ServantNotActive, org.omg.CosNaming.NamingContextPackage.InvalidName, NotFound, CannotProceed
+    public static void installRemoteObjectAtLocalhost(Servant servant,
+        String name)
+        throws InvalidName, AdapterInactive, WrongPolicy, ServantNotActive,
+        org.omg.CosNaming.NamingContextPackage.InvalidName, NotFound,
+        CannotProceed
     {
-        String[] args = {"-ORBInitialPort", "3000"};
+        String[] args =
+                        {"-ORBInitialPort", "3000"};
 
         // ORB initialisieren
         final ORB orb = ORB.init(args, null);
@@ -61,7 +66,8 @@ public class CorbaManager
         root.the_POAManager().activate();
 
         // Namensdienst ermitteln
-        NamingContextExt naming = NamingContextExtHelper.narrow(orb.resolve_initial_references("NameService")); // (NamingContextExt)
+        NamingContextExt naming = NamingContextExtHelper.narrow(orb.
+            resolve_initial_references("NameService")); // (NamingContextExt)
 
         // Referenz auf Counter Objekt unter "OurFloatAdd" registrieren
         org.omg.CORBA.Object serviceRef = root.servant_to_reference(servant);
