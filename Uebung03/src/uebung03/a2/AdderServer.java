1dcc0a981509937288cc42cdb25a73a080dc13ba
secos <secos>
2004-01-05 21:40:47 +0000

    Funktioniert

---
+++ Uebung03/src/uebung03/a2/AdderServer.java
@@ -0,0 +1,66 @@
+package uebung03.a2;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class AdderServer
+extends ServerSocket
+{
+
+    //  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+    //  |                   Constructors                    |   \\
+    //  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
+
+    public AdderServer(int port)
+    throws IOException
+    {
+        super(port);
+    }
+
+    //  | = - = - = - = - = - /-||=||-\ - = - = - = - = - = |   \\
+    //  |                Modifying Methods                  |   \\
+    //  | = - = - = - = - = - \-||=||-/ - = - = - = - = - = |   \\
+
+    /**
+     * Listens to the given port.
+     * Any connecting client is passed to an AdderService in a single Thread -
+     * allowing multiple connections.
+     */
+    public void start()
+    {
+        while (true)
+        {
+            try
+            {
+                // detect client
+                final Socket client = accept();
+
+                // start a new Thread which performs the actual work (multithreaded server)
+                new Thread(new Runnable()
+                {
+                    public void run()
+                    {
+                        try
+                        {
+                            new AdderService(client);
+                        }
+                        catch (IOException e)
+                        {
+                            System.err.println(e.toString());
+                        }
+                    }
+
+                }).start();
+            }
+            catch (IOException e)
+            {
+                System.err.println(e.toString());
+            }
+
+            // and continue listening for new Clients
+        }
+
+
+    }
+}
