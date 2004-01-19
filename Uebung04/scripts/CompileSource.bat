d1592272a1a276f0ba8184feaff7bd5f2f28eda3
secos <secos>
2004-01-19 13:53:01 +0000

    <Kein Kommentar eingegeben>

--- Uebung04/scripts/CompileSource.bat
+++ Uebung04/scripts/CompileSource.bat
@@ -1,6 +1,6 @@
 mkdir    ..\classes
 
-javac -source 1.4 -d ..\classes -sourcepath ..\src ..\src\uebung03\*.java
-javac -source 1.4 -d ..\classes -sourcepath ..\src ..\src\uebung03\a1\*.java
-javac -source 1.4 -d ..\classes -sourcepath ..\src ..\src\uebung03\a2\*.java
-javac -source 1.4 -d ..\classes -sourcepath ..\src ..\src\uebung03\a3\*.java
+javac -source 1.4 -d ..\classes -sourcepath ..\src ..\src\uebung04\a1\*.java
+javac -source 1.4 -d ..\classes -sourcepath ..\src ..\src\uebung04\a2\*.java
+javac -source 1.4 -d ..\classes -sourcepath ..\src ..\src\uebung04\a3\*.java
+javac -source 1.4 -d ..\classes -sourcepath ..\src ..\src\uebung04\a4\*.java
--- Uebung04/scripts/a2/LinkChecker.bat
+++ Uebung04/scripts/a2/LinkChecker.bat
@@ -1,1 +1,1 @@
-cmd /k java -cp ..\..\classes uebung03.a2.StartClient 127.0.0.1 7777 281111111 3
+cmd /k java -cp ..\..\classes uebung04.a2.Linkchecker ..\..\libs\a2\urls.txt
