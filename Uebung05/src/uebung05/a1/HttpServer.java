db7d2ba013476ad498582b513a204eea4709bdf9
secos <secos>
2004-01-23 17:24:29 +0000

    <Kein Kommentar eingegeben>

---
+++ Uebung05/src/uebung05/a1/HttpServer.java
@@ -0,0 +1,82 @@
+package uebung05.a1;
+
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+
+public class HttpServer
+implements Runnable
+{
+
+	private ServerSocket socket;
+	private HashMap handlers = new HashMap();
+
+	public HttpServer()
+	throws IOException
+	{
+		this(80);
+	}
+
+	public HttpServer(int port)
+	throws IOException
+	{
+		socket = new ServerSocket(port);
+	}
+
+	public void run()
+	{
+		while (true)
+		{
+			try
+			{
+				Socket client = socket.accept();
+
+				String requestString = readRequest(client);
+				ClientRequest request = new ClientRequest(requestString);
+
+				if (request.getSessionID().equals("-1") || !handlers.containsKey(request.getSessionID()))
+				{
+					RequestHandler handler = new RequestHandler();
+					request.setSessionID(handler.toString());
+					handlers.put(handler.toString(), handler);
+				}
+
+				String response = ((RequestHandler) handlers.get(request.getSessionID())).createResponse(request);
+
+				BufferedWriter ou = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+				ou.write(response);
+				ou.flush();
+
+				client.close();
+			}
+			catch (IOException e)
+			{
+				System.err.println(e.toString());
+			}
+		}
+	}
+
+	private String readRequest(Socket client)
+	throws IOException
+	{
+		client.setSoTimeout(100);
+
+		BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+		String request = "";
+
+		while (true)
+		{
+			try
+			{
+				request += input.readLine() + "\n";
+			}
+			catch (SocketTimeoutException e)
+			{
+				break;
+			}
+		}
+
+		return request;
+	}
+}
--- Uebung05/src/uebung05/a1/StartServer.java
+++ Uebung05/src/uebung05/a1/StartServer.java
@@ -4,29 +4,9 @@ import java.io.IOException;
 
 public class StartServer
 {
-	/**
-	 * Starts the HttpServer which listens for client connections.
-	 *
-	 * @param args start at given port - optional (default: 80)
-	 * @throws IOException
-	 */
 	public static void main(String[] args)
 	throws IOException
 	{
-		int port = 80;
-
-		if (args.length == 1)
-		{
-			try
-			{
-				port = Integer.parseInt(args[0]);
-			}
-			catch (NumberFormatException e)
-			{
-				System.err.println(e.toString());
-			}
-		}
-
-		new HttpAddServer(port).start();
+		new Thread(new HttpServer()).start();
 	}
 }
